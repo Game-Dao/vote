e4a3c7a79c0a86edf5d60963e2ba396fb154fc6a
andy zhang <andy@wild-dynasty.com>
2023-12-01 10:11:08 +0800

    feat: localstorage resolve

--- app/hooks/useVoteTab.ts
+++ app/hooks/useVoteTab.ts
@@ -31,15 +31,25 @@
 // export default useVoteTab
 
 
-
-
 import { useState, useEffect } from 'react';
 
 function useVoteTab(): { handleTabChange: (newTab: string) => void; tab: string } {
-  const [tab, setTab] = useState<string>(() => localStorage?.getItem('tab') || '1');
+  // 初始状态设置为 '1'，然后在客户端中从 localStorage 加载真实值
+  const [tab, setTab] = useState<string>('1');
 
   useEffect(() => {
-    localStorage.setItem('tab', tab);
+    // 检查是否在浏览器环境中
+    if (typeof window !== 'undefined') {
+      const storedTab = localStorage.getItem('tab') || '1';
+      setTab(storedTab);
+    }
+  }, []);
+
+  useEffect(() => {
+    // 同样检查是否在浏览器环境中
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('tab', tab);
+    }
   }, [tab]);
 
   const handleTabChange = (newTab: string) => {
@@ -53,4 +63,3 @@ function useVoteTab(): { handleTabChange: (newTab: string) => void; tab: string 
 }
 
 export default useVoteTab;
-
